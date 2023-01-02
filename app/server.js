2f516f2a9ec0779b92c67139e34d10edbfc0dad1
Pravinsinh <pravindesaicdac@gamil.com>
2023-01-02 16:44:38 +0530

    m

--- app/server.js
+++ app/server.js
@@ -2,8 +2,7 @@ const express=require('express')
 
 app=express()
 
-const path=require('path');
-app.use(express.static(path.join(__dirname,"public")))
+
 
 app.get("/",(req,res)=>{
     app.sendfile("public/login.html",{root:__dirname})
